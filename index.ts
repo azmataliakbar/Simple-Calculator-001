5682cbd640e7a18cdcb577388e4e31922d06a50a
azmataliakbar <azmat36@yahoo.com>
2024-03-23 16:30:39 +0500

    changed version and modified

--- index.ts
+++ index.ts
@@ -1,18 +1,14 @@
 #! /usr/bin/env node
 
-
 import chalk from "chalk";
 
 import inquirer from "inquirer";
 
-// async function calculator() {
-  
-
 const answer = await inquirer.prompt([
-  {message: chalk.bgRedBright.bold.whiteBright("Enter first number: "), type: "number", name: "firstNumber"},
-  {message: chalk.bgCyanBright.bold.whiteBright("Enter second number: "), type: "number", name: "secondNumber"},
+  {message: chalk.italic.bold.greenBright("Enter first number: "), type: "number", name: "firstNumber"},
+  {message: chalk.italic.bold.cyanBright("Enter second number: "), type: "number", name: "secondNumber"},
   {
-    message: chalk.bold.red("Select one of the operators to perform operation"),
+    message: chalk.bold.italic.redBright("Select one of the operators to perform operation"),
     type: "list",
     name: "operator",
     choices: ["Addition", "Subtraction", "Multiplication", "Division"],
@@ -22,19 +18,15 @@ const answer = await inquirer.prompt([
 
 // conditional statement
 if (answer.operator === "Addition") {
-  console.log(chalk.bgGreenBright.bold.whiteBright(answer.firstNumber + answer.secondNumber));
+  console.log(chalk.italic.bold.yellowBright(answer.firstNumber + answer.secondNumber));
 } else if (answer.operator === "Subtraction") {
-  console.log(chalk.bgGreenBright.bold.whiteBright(answer.firstNumber - answer.secondNumber));
+  console.log(chalk.italic.bold.yellowBright(answer.firstNumber - answer.secondNumber));
 } else if (answer.operator === "Multiplication") {
-  console.log(chalk.bgGreenBright.bold.whiteBright(answer.firstNumber * answer.secondNumber));
+  console.log(chalk.italic.bold.yellowBright(answer.firstNumber * answer.secondNumber));
 } else if (answer.operator === "Division") {
-  console.log(chalk.bgGreenBright.whiteBright.bold(answer.firstNumber / answer.secondNumber));
+  console.log(chalk.italic.bold.yellowBright(answer.firstNumber / answer.secondNumber));
 } else {
-  console.log(chalk.red("Please select a valid operator"));
+  console.log(chalk.italic.bold.red("Please select a valid operator"));
   
 }
-// }
 
-// calculator();
-
-//console.log(answer);
