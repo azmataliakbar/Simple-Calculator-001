a5b2ba4a256bf317ea506dc217de2b1ca03435a9
azmataliakbar <azmat36@yahoo.com>
2024-03-29 19:59:39 +0500

    refreshed

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 #! /usr/bin/env node
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const chalk_1 = __importDefault(require("chalk"));
-const inquirer_1 = __importDefault(require("inquirer"));
-const answer = await inquirer_1.default.prompt([
-    { message: chalk_1.default.italic.bold.greenBright("Enter first number: "), type: "number", name: "firstNumber" },
-    { message: chalk_1.default.italic.bold.cyanBright("Enter second number: "), type: "number", name: "secondNumber" },
+import chalk from "chalk";
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: chalk.italic.bold.greenBright("Enter first number: "), type: "number", name: "firstNumber" },
+    { message: chalk.italic.bold.cyanBright("Enter second number: "), type: "number", name: "secondNumber" },
     {
-        message: chalk_1.default.bold.italic.redBright("Select one of the operators to perform operation"),
+        message: chalk.bold.italic.redBright("Select one of the operators to perform operation"),
         type: "list",
         name: "operator",
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
@@ -18,18 +13,18 @@ const answer = await inquirer_1.default.prompt([
 ]);
 // conditional statement
 if (answer.operator === "Addition") {
-    console.log(chalk_1.default.italic.bold.yellowBright(answer.firstNumber + answer.secondNumber));
+    console.log(chalk.italic.bold.yellowBright(answer.firstNumber + answer.secondNumber));
 }
 else if (answer.operator === "Subtraction") {
-    console.log(chalk_1.default.italic.bold.yellowBright(answer.firstNumber - answer.secondNumber));
+    console.log(chalk.italic.bold.yellowBright(answer.firstNumber - answer.secondNumber));
 }
 else if (answer.operator === "Multiplication") {
-    console.log(chalk_1.default.italic.bold.yellowBright(answer.firstNumber * answer.secondNumber));
+    console.log(chalk.italic.bold.yellowBright(answer.firstNumber * answer.secondNumber));
 }
 else if (answer.operator === "Division") {
-    console.log(chalk_1.default.italic.bold.yellowBright(answer.firstNumber / answer.secondNumber));
+    console.log(chalk.italic.bold.yellowBright(answer.firstNumber / answer.secondNumber));
 }
 else {
-    console.log(chalk_1.default.italic.bold.red("Please select a valid operator"));
+    console.log(chalk.italic.bold.red("Please select a valid operator"));
 }
-console.log(chalk_1.default.italic.bold.redBright("\nThe End"));
+console.log(chalk.italic.bold.redBright("\nThe End"));
